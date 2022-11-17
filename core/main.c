527dda812840dc35ba8e05e20068961568bf46d4
Tanay Mehta <mehtatanay30@gmail.com>
2022-11-17 15:30:36 +0530

    improved version of binary tree2

--- core/main.c
+++ core/main.c
@@ -2,8 +2,12 @@
 #include "structures.h"
 
 int main(void){
-    // string filepath_data = "../data/data.csv";
+    string filepath_data = "../data/data.csv";
     // string filepath_tables = "../data/tables.csv";
     // string filepath_id = "../data/id.txt";
     // string temp_file = "../data/temp.txt";
+    //goal = find dish id 7
+
+    int hello = find_item_BT(filepath_data, 2);
+    printf("%i", hello);
 }
