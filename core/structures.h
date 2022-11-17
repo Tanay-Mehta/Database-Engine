c42e2e89f2fd2416d8ddede00e39edea1d266dcb
Tanay Mehta <mehtatanay30@gmail.com>
2022-11-17 13:59:48 +0530

    improved version of binary tree

--- core/structures.h
+++ core/structures.h
@@ -34,7 +34,12 @@ typedef struct ride{
     string user;
     date date;
 }ride;
+typedef struct keyval{
+    int id;
+    int dish;
+}keyval;
 
+typedef struct keyval list[10];
 typedef struct general{
     order order;
     ride ride;
@@ -44,5 +49,6 @@ string stringify_order(order order);
 int parse_date(string date2, struct date date1);
 int parse_order(string order2, struct order order1);
 int find_item_BT(string tablename_data, int id);
+node* sortedArrayToBST(list order, int start, int end);
 
 #endif
